c7d867edf0a56d738547e4ddb37ec99cd40a2527
Robin Modisch <robin.modisch@fau.de>
2019-11-06 13:50:45 +0100

    refine PrintPhoto tests

--- src/test/java/org/wahlzeit/model/PrintPhotoTest.java
+++ src/test/java/org/wahlzeit/model/PrintPhotoTest.java
@@ -1,28 +1,36 @@
 package org.wahlzeit.model;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
+import org.wahlzeit.testEnvironmentProvider.LocalDatastoreServiceTestConfigProvider;
+import org.wahlzeit.testEnvironmentProvider.RegisteredOfyEnvironmentProvider;
+import org.wahlzeit.testEnvironmentProvider.UserSessionProvider;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertNotEquals;
 
 public class PrintPhotoTest {
 
     @Test()
     public void testConstructor() {
         PrintPhoto photo = new PrintPhoto(PrintPhoto.Material.PETG);
+        assertEquals(PrintPhoto.Material.PETG, photo.getPrintMaterial());
     }
 
-    @Test(expected = ExceptionInInitializerError.class)
+/*    @Test(expected = ExceptionInInitializerError.class)
     public void testInvalidConstructor() {
         PrintPhoto photo = new PrintPhoto(PrintPhoto.Material.PETG);
         fail("Construction was successful but should throw an error");
-    }
+    }*/
 
     @Test
-    public void testGetPrintMaterial() {
+    public void testNotEqualMaterial() {
         PrintPhoto photo = new PrintPhoto(PrintPhoto.Material.PETG);
+        PrintPhoto photo2 = new PrintPhoto(PrintPhoto.Material.PETG);
 
-        assertEquals(PrintPhoto.Material.PETG, photo.getPrintMaterial());
+        assertNotEquals(photo, photo2);
     }
 
 }
