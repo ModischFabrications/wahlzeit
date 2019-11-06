c6109550f0991cfab95bd944bbbac338be2b16ac
Robin Modisch <robin.modisch@fau.de>
2019-11-06 18:32:01 +0100

    fix appEngine Init for Tests in Manager

--- src/test/java/org/wahlzeit/model/PrintPhotoManagerTest.java
+++ src/test/java/org/wahlzeit/model/PrintPhotoManagerTest.java
@@ -1,13 +1,30 @@
 package org.wahlzeit.model;
 
 import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
+import org.wahlzeit.testEnvironmentProvider.*;
 
 import java.io.IOException;
 
 import static org.junit.Assert.assertTrue;
 
 public class PrintPhotoManagerTest {
+
+    @ClassRule
+    public static SysConfigProvider sysConfigProvider = new SysConfigProvider();
+    @ClassRule
+    public static WebFormHandlerProvider webFormHandlerProvider = new WebFormHandlerProvider();
+
+    @Rule
+    public TestRule chain = RuleChain.
+            outerRule(new LocalDatastoreServiceTestConfigProvider()).
+            around(new RegisteredOfyEnvironmentProvider()).
+            around(new UserSessionProvider());
+
     private PrintPhotoManager manager;
 
     @Before
@@ -36,7 +53,9 @@ public class PrintPhotoManagerTest {
     }
 
     @Test
-    public void testGetHasPhoto() {
+    public void testGetHasPhoto() throws IOException {
+        manager.addPhoto(new PrintPhoto(PrintPhoto.Material.PETG));
+
         PrintPhoto photo = manager.getPhoto(PhotoId.getRandomId());
 
         assertTrue(manager.hasPhoto(photo.id));
