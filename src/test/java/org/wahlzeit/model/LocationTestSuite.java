58df5c364bbd028cbd1016f8acc281807e16500c
Robin Modisch <robin.modisch@fau.de>
2019-11-15 21:13:13 +0100

    move coordinates to separate subpackage

--- src/test/java/org/wahlzeit/model/LocationTestSuite.java
+++ src/test/java/org/wahlzeit/model/LocationTestSuite.java
@@ -2,6 +2,10 @@ package org.wahlzeit.model;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import org.wahlzeit.model.coordinate.CartesianCoordinateTest;
+import org.wahlzeit.model.coordinate.CoordinateTest;
+import org.wahlzeit.model.coordinate.LocationTest;
+import org.wahlzeit.model.coordinate.SphericCoordinateTest;
 
 @RunWith(Suite.class)
 
