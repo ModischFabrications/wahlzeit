b6e53621e7d121fcf5f01f8148be0a07a5efd637
Robin Modisch <robin.modisch@fau.de>
2019-12-16 19:55:03 +0100

    make pattern repeatable

--- src/main/java/org/wahlzeit/utils/PatternInstance.java
+++ src/main/java/org/wahlzeit/utils/PatternInstance.java
@@ -1,7 +1,15 @@
 package org.wahlzeit.utils;
 
+import java.lang.annotation.Repeatable;
+
+@Repeatable(PatternInstance.List.class)
 public @interface PatternInstance {
     String patternName();
 
     String[] participants();
+
+    // needed to be repeatable
+    @interface List {
+        PatternInstance[] value();
+    }
 }
